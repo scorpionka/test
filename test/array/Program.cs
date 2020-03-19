b85e8b85f3ca4fe6919f48b0ad3e509579e54a8e
Алексей Кузьмицкий <a.kuzmitsky@gmail.com>
2020-03-19 21:23:49 +0300

    commit190320

---
+++ test/array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[50];
+            Console.WriteLine(data[5]);
+            int[] data_1 = new int[4] { 1, 2, 4, 6 };
+            Program[] programs = new Program[56];
+        }
+    }
+}
---
+++ test/switch ver7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace switch_ver7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> collection = new List<object>();
+            collection.Add(43);
+            collection.Add(DayOfWeek.Monday);
+            collection.Add("Hello, guys!");
+            collection.Add(null);
+            collection.Add(false);
+            collection.Add("Hello, world!");
+            foreach (var item in collection)
+            {
+                switch (item)
+                {
+                    case string text when text.Equals("Hello, guys!"):
+                        Console.WriteLine(text);
+                        break;
+                    case null:
+                        Console.WriteLine("Null");
+                        break;
+                    case Boolean @bool:
+                        Console.WriteLine(@bool);
+                        break;
+                    case int num when num == 43:
+                        Console.WriteLine(num);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
