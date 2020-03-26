f1eb3b6591e2b13038c0d4eee05fa0ea3e42c330
Алексей Кузьмицкий <a.kuzmitsky@gmail.com>
2020-03-26 21:19:08 +0300

    commit StringBuilder

---
+++ test/StringBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilder_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(DateTime.UtcNow);
+            sb.Append('\n');
+            sb.Append("Hello!");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
