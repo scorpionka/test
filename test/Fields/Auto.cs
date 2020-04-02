3a64427ac648dc3a8f27ad780d1b4d693927ab42
Алексей Кузьмицкий <a.kuzmitsky@gmail.com>
2020-04-02 20:49:22 +0300

    Method

---
+++ test/Fields/Auto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fields
+{
+    class Auto
+    {
+        public int Distance { get; set; }
+
+        public string Model { get; set; }
+    }
+}
---
+++ test/Fields/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto car = new Auto() { Distance = 150, Model = "Honda" };
+            Console.WriteLine(car.Distance);
+            car.Distance = 50;
+            CleanAuto(ref car);
+            Console.WriteLine(car.Distance);
+        }
+
+        static void CleanAuto(ref Auto car)
+        {
+            car.Distance = 100;
+            car = new Auto();
+        }
+    }
+}
